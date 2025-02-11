729f80ace68324c1dd20357c7695ed4e34d8affd
Hamza-Idrees <hamzaidrees083@gmail.com>
2025-02-11 16:29:59 +0500

    add github authentication

--- redis/redisConnection.js
+++ redis/redisConnection.js
@@ -1,8 +1,11 @@
 const redis = require("redis");
 require("dotenv").config();
 
+const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+const REDIS_PORT = process.env.REDIS_PORT || "6379";
+
 const client = redis.createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`
 });
 
 client.on("connect", () => {
