81a6cb0c833a2e6c9446857ff5fde2d4cc53cc81
Hamza-Idrees <hamzaidrees083@gmail.com>
2025-02-11 17:38:58 +0500

    Fixed Redis startup issues

--- redis/redisConnection.js
+++ redis/redisConnection.js
@@ -5,15 +5,19 @@ const REDIS_HOST = process.env.REDIS_HOST || "todo-redis";
 const REDIS_PORT = process.env.REDIS_PORT || "6379";
 
 const client = redis.createClient({
-  url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+  retry_strategy: (options) => {
+    console.log(`Redis retrying connection... Attempt ${options.attempt}`);
+    return Math.min(options.attempt * 100, 3000); // Retry every few seconds
+  }
 });
 
 client.on("connect", () => {
-  console.log("Redis Connected");
+  console.log("✅ Redis Connected!");
 });
 
 client.on("error", (err) => {
-  console.log("Redis error: ", err);
+  console.error("❌ Redis error: ", err);
 });
 
 client.connect();
